e254ca2bbf8833e08a7d87e33e82661b948e9e0c
njteh7 <njteh77@hotmail.com>
2018-03-14 14:21:08 -0400

    Week 7

--- Week2/Assets/scripts/DestroyByContact.cs
+++ Week2/Assets/scripts/DestroyByContact.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
     public GameObject explosionAsteroid;
+    public GameObject explosionPlayer;
+    public int scoreValue = 10;
+
+    private GameController gameControllerScript;
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+
+        if (gameControllerObject != null)
+        {
+            gameControllerScript = gameControllerObject.GetComponent<GameController>();
+        }
+
+        if (gameControllerScript == null)
+        {
+            Debug.Log("Cannot find Game Controller script on object");
+        }
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Boundary")
@@ -17,7 +32,23 @@ public class DestroyByContact : MonoBehaviour {
             //Debug.Log("DestroyByContact");
         }
 
-        Instantiate(explosionAsteroid, this.transform.position, this.transform.rotation);
+        if (other.tag == "Player")
+        {
+            Vector3 deltaP = (this.transform.position + other.transform.position) / 2;  // Vector between player and astroid
+            // deltaP = (other.transform.position + deltaP) / 2; // Find halfway point between both objects
+            // Create our explosion animation
+            Instantiate(explosionPlayer, other.transform.position, other.transform.rotation);
+
+            // GAME OVER!
+            gameControllerScript.GameOver();
+        }
+        else
+        {
+            Instantiate(explosionAsteroid, this.transform.position, this.transform.rotation);
+            gameControllerScript.AddScore(scoreValue);
+        }
+
+        
 
         Destroy(this.gameObject); // Destroying this thing (laser)
         Destroy(other.gameObject); // Destroying this thing (the astroid)
---
+++ Week2/Assets/scripts/GameController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard; // What are the spawning
+    public Vector2 spawnValue; // Where do we spawn our hazards
+    public int hazardCount; // How many hazards per wave
+    public float startWait; // How long until the next wave
+    public float spawnWait; // How long between each hazard in each wave?
+    public float waveWait; // How long between each wave of enemies?
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+	// Use this for initialization
+	void Start ()
+    {
+        score = 0;
+        StartCoroutine(SpawnWaves());
+    }
+		
+	
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait); // Pause. How long do we wait for the first wave?
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector2 spawnPosition = new Vector2(spawnValue.x, Random.Range(-spawnValue.y, spawnValue.y));
+                //                                      12                         -3.5            3.5
+                Quaternion spawnRotation = Quaternion.identity; // Default rotation
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait); // Wait time between spawning each asteroid
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                break;
+            }
+        }
+    }
+	
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        Debug.Log("Score is " + score);
+    }
+    
+    public void GameOver()
+    {
+        Debug.Log("Game is Over");
+        gameOver = true;
+    }
+
+}
